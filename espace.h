686495de13b98bdd24d088e262311d93cf657cf9
Kylaste <kylaste@yahoo.fr>
2016-05-18 20:22:00 +0200

    Update espace.h

--- espace.h
+++ espace.h
@@ -16,7 +16,7 @@ void affichageEspace(const Espace * const);
 
 int estDedans(const Espace * const, const int, const int);
 
-void decouperEspace(Espace * const e, int * const retour);
+void decouperEspace(Espace * const e, int* identifiant, int * const retour);
 
 int estVoisin(const Espace * const, const int, const int, const int, const int);
 
